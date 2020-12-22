636e2140f9dd5bfeffe784d0f563ca714d4d8c92
Matt Shields <matt@shields.tv>
2020-12-22 11:30:36 -0500

    terminal

--- theme/icons/init.lua
+++ theme/icons/init.lua
@@ -3,7 +3,7 @@ local dir = os.getenv('HOME') .. '/.config/awesome/theme/icons'
 return {
   --tags
   chrome = dir .. '/google-chrome.svg',
-  terminal = dir .. '/termius-app.svg',
+  terminal = dir .. '/Terminal.svg',
   code = dir .. '/code-braces.svg',
   social = dir .. '/forum.svg',
   folder = dir .. '/folder.svg',
