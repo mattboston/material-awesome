7169a0e54e2f15d2b321366c38b4079705b2d343
Matt Shields <matt@shields.tv>
2020-12-22 11:28:58 -0500

    terminal

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -11,6 +11,12 @@ local tags = {
     screen = 1
   },
   {
+    icon = icons.terminal,
+    type = 'terminal',
+    defaultApp = apps.default.browser,
+    screen = 1
+  },
+  {
     icon = icons.code,
     type = 'code',
     defaultApp = apps.default.editor,
